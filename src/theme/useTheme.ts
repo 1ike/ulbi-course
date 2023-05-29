3796b7810050cfd3980565eec9eee86d11f688d2
1ike <googlohren@gmail.com>
2023-05-29 17:46:07 +0300

    8.1: add custom classNames.

--- src/theme/useTheme.ts
+++ src/theme/useTheme.ts
@@ -8,16 +8,15 @@ interface UseThemeResult {
 
 export function useTheme(): UseThemeResult {
     const {theme, setTheme} = useContext(ThemeContext);
-    console.log('theme = ', theme);
 
     const toggleTheme = () => {
         const newTheme = theme === Theme.DARK ? Theme.LIGHT : Theme.DARK
         setTheme(newTheme);
-        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme);
+        localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
     }
 
     return {
         theme,
-        toggleTheme,
+        toggleTheme
     }
 }
