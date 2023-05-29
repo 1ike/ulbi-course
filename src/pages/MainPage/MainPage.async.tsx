ee6bfb902730087b2de33df2d3c6004ba748fccf
1ike <googlohren@gmail.com>
2023-05-29 17:25:17 +0300

    7.1: add themes.

--- src/pages/MainPage/MainPage.async.tsx
+++ src/pages/MainPage/MainPage.async.tsx
@@ -5,4 +5,3 @@ export const MainPageAsync = lazy(() => new Promise(resolve => {
     // ТАК В РЕАЛЬНЫХ ПРОЕКТАХ НЕ ДЕЛАТЬ!!!!! ДЕЛАЕМ ДЛЯ КУРСА!
     setTimeout(() => resolve(import('./MainPage')), 1500)
 }))
-
